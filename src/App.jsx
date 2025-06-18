0352ab1fdb373ef696ed70bb025b755441e17c3d
shraddhabhikadiya2017 <shraddhabhikadiya@2017.com>
2025-06-18 09:20:37 +0200

    Added postdetails

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router";
 
 import { MainLayout } from "./layouts/MainLayout";
 import { Homepage } from "./pages/index";
+import { PostDetails } from "./pages/PostDetails";
 
 const App = () => {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Homepage />} />
+        <Route path="/posts/:id" element={<PostDetails />} />
       </Route>
     </Routes>
   );
